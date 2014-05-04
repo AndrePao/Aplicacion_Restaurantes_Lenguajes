909f62adf7c4b811502ce0d5327a717260ae9ff8
Veronica <ver01raq@gmail.com>
2014-05-04 10:27:21 -0600

    Formato de la base de conocimientos

--- Base_de_conocimientos.pl
+++ Base_de_conocimientos.pl
@@ -1,17 +1,10 @@
-restaurante(banquetes,hawaiana, san_diego,22334455,sabado_lunes,7000).
-restaurante(mussio,chatarra, tres_rios,22334455,lunes_sabado,32090).
-restaurante(mc_donalds,chatarra, san_jose,22334455,lunes_viernes,4006).
-restaurante(kfc,chatarra, terramall,22334455,domingo_viernes,40045).
+restaurante(banquetes,hawaiana, san_diego,22334455,sabado_lunes,07001000, [pizza,papas]).
+restaurante(mussio,chatarra, tres_rios,22334455,lunes_sabado,03200789,[hamburguesa,papas]).
+restaurante(mc_donalds,chatarra, san_jose,22334455,lunes_viernes,0400600,[mcnifica,papas]).
+restaurante(kfc,chatarra, terramall,22334455,domingo_viernes,0400415,[popcorn,papas]).
+restaurante(pan_e_vino,italiana, terramall,89497239,domingo_viernes,04000645,[pizza,lasagna,chiverre]).
 
+platillos(banquetes,galleta,salado,costarica,[harina,pina,queso]).
 platillos(banquetes,pizza,salado,francia,[pan,pina,queso]).
 platillos(kfc,wrap,salado,francia,[tortilla,pollo,lechuga,tomate,queso]).
-
-listarestaurantes(NombreR):-restaurante(NombreR,_,_,_,_,_).
-restaurantes_tipo_comida(NombreR,TipoComida):-restaurante(NombreR,TipoComida,_,_,_,_).
-buscar_restaurante(NombreR,TipoComida,Lugar,Tel,DiasAtencion, HorasAtencion2):-restaurante(NombreR,TipoComida,Lugar,Tel,DiasAtencion, HorasAtencion2).
-
-restaurantes_pais(NombreR,NombrePais):- platillos(NombreR,_,_,NombrePais,_).
-platillos_restaurantes(NombreR,NombrePlatillo):-platillos(NombreR,NombrePlatillo,_,_,_).
-
-ingrediente_platillos_restaurante(NombreR,NombrePlatillo,Ingrediente):-platillos(NombreR,NombrePlatillo,_,_,[IngredienteL|ListaI]), member(Ingrediente,[IngredienteL|ListaI]).
-platillos_pais(nombrep,nombrepais):-platillos(_,nombrep,_,nombrepais,_).
+platillos(crustaseo_cascarudo,cangreburguer,salado,fondo_de_bikini,[pan,lechuga,tomate,pepinillos,carne]).
